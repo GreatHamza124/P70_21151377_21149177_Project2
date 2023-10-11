91dcab7cacd26833145699100709c2cefb1b7f88
Hamza Hussain <wct8670@autuni.ac.nz>
2023-10-11 15:16:55 +1300

    I made my 2nd commit

--- hamza_yahya_project/src/hamza_yahya_project/Hamza_yahya_project.java
+++ hamza_yahya_project/src/hamza_yahya_project/Hamza_yahya_project.java
@@ -14,7 +14,9 @@ public class Hamza_yahya_project {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String name = "hamza";
+        System.out.println("hello" + name);
+                
     }
     
 }
